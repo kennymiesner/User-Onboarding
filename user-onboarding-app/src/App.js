6f2f70a6c5ec72758751d90d4b4aeb9cb1ce478f
kennymiesner <kennymiesner@gmail.com>
2021-07-21 16:26:24 -0500

    Created advanced form

--- user-onboarding-app/src/App.js
+++ user-onboarding-app/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import newHire from './components/NewHire'
 import Form from './components/Form'
+import schema from './validation/formSchema'
+import { reach } from 'yup'
+import axios from 'axios'
 import './App.css';
+
+const initialFormValues = {
+  ///// TEXT INPUTS /////
+  name: '',
+  email: '',
+  password: '',
+  terms: false,
+}
+
+const initialEmployees = []
+const initialDisabled = true
+
+const initialFormErrors = {
+  username: '',
+  email: '',
+  password: '',
+  terms: '',
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form />
     </div>
   );
 }
