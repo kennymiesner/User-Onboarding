50840776f9301a02f7fe28c6c490c1f0382cbddc
kennymiesner <kennymiesner@gmail.com>
2021-07-22 14:48:42 -0500

    Set up individual field tests, as well as full form test

---
+++ user-onboarding-app/cypress/integration/form_test.js
@@ -0,0 +1,56 @@
+const nameInput = () => cy.get('input[name=name]')
+const emailInput = () => cy.get('input[name=email]')
+const passwordInput = () => cy.get('input[name=password]')
+const termsCheckbox = () => cy.get('input[name=terms]')
+const submitBtn = () => cy.get('button')
+
+describe('User Onboarding App Tests', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Test Name Input', () => {
+    nameInput()
+      .type('Kenny Miesner')
+      .should('have.value', 'Kenny Miesner')
+  })
+
+  it('Test Email Input', () => {
+    emailInput()
+      .type('kennymiesner@gmail.com')
+      .should('have.value', 'kennymiesner@gmail.com')
+  })
+
+  it('Test Password Input', () => {
+    passwordInput()
+      .type('#15Kanon')
+      .should('have.value', '#15Kanon')
+  })
+
+  it('Test Checkbox', () => {
+    termsCheckbox()
+      .check()
+      .uncheck()
+  })
+
+  it('Test Submit Button', () => {
+    submitBtn()
+      .should('be.disabled')
+  })
+
+  it('Fill and submit form successfully', () => {
+    nameInput()
+      .type('Kenny Miesner')
+      .should('have.value', 'Kenny Miesner')
+    emailInput()
+      .type('kennymiesner@gmail.com')
+      .should('have.value', 'kennymiesner@gmail.com')
+    passwordInput()
+      .type('#15Kanon')
+      .should('have.value', '#15Kanon')
+    termsCheckbox()
+      .check()
+    submitBtn()
+      .should('be.enabled')
+  })
+})
