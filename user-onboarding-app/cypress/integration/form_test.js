295303c92334a8421b9430fc026395a10db2d293
kennymiesner <kennymiesner@gmail.com>
2021-07-22 15:02:29 -0500

    Completed MVP

--- user-onboarding-app/cypress/integration/form_test.js
+++ user-onboarding-app/cypress/integration/form_test.js
@@ -3,6 +3,7 @@ const emailInput = () => cy.get('input[name=email]')
 const passwordInput = () => cy.get('input[name=password]')
 const termsCheckbox = () => cy.get('input[name=terms]')
 const submitBtn = () => cy.get('button')
+const errors = () => cy.get('.errors')
 
 describe('User Onboarding App Tests', () => {
   beforeEach(() => {
@@ -52,5 +53,17 @@ describe('User Onboarding App Tests', () => {
       .check()
     submitBtn()
       .should('be.enabled')
+      .click()
+    cy.contains('Kenny Miesner')
+  })
+
+  it('Validate unfilled form field', () => {
+    nameInput()
+      .type('Kenny Miesner')
+      .should('have.value', 'Kenny Miesner')
+    emailInput()
+      .type(' ')
+      .should('have.value', ' ')
+    errors().should('exist')
   })
 })
